607db0b49a1c6d73dbe31de882c3e05eee6abf32
haniiw <hokinethk@gmail.com>
2025-09-04 18:22:33 +0700

    PWL-KELOMPOK-8/homepage.php

---
+++ homepage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JEF TEAM</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header class="navbar">
+    <div class="logo">
+      <span>JEF Cinema</span>
+    </div>
+    <div class="auth-btn">
+      <button class="btn login">Log In</button>
+      <button class="btn signup">Sign Up</button>
+    </div>
+  </header>
+
+  <section class="hero">
+    <h1>FEEL THE MOMENTS BEYOND</h1>
+    <div class="search-box">
+      <input type="text" placeholder="Search">
+      <span class="search-icon">🔍</span>
+    </div>
+
+    <div class="menu-icons">
+      <div class="menu-item">
+        <img src="fotohomepage/bioskop.png" alt="Bioskop">
+        <p>Bioskop</p>
+      </div>
+      <div class="menu-item">
+        <img src="fotohomepage/film (1).png" alt="Film">
+        <p>Film</p>
+      </div>
+      <div class="menu-item">
+        <img src="fotohomepage/foodiez (1).png" alt="Foodiez">
+        <p>Foodiez</p>
+      </div>
+      <div class="menu-item">
+        <img src="fotohomepage/reservation (1).png" alt="Reservation">
+        <p>Reservation</p>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
